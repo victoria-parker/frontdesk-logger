faba663ce073fda1937e49efc99472c7ac78c1a9
victoria-parker <victoriaparkerb@gmail.com>
2022-12-29 11:14:50 +0000

    dependencies added

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const passport=require("passport")
+const session=require("express-session")
+const MongoStore=require("connect-mongo")(session)
+const methodOverride=require("method-override")
+const flash=require("express-flash")
