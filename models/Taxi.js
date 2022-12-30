7530546431efa6b8131202c45aa9666961d31d75
victoria-parker <victoriaparkerb@gmail.com>
2022-12-30 13:53:18 +0000

    taxi model done

---
+++ models/Taxi.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const TaxiSchema = new mongoose.Schema({
+    roomNumber:{
+        type: Number,
+        required:true
+    },
+    pickUpTime:{
+        type: Date,
+        required: true,
+    },
+    bookingNumber:{
+        type: Number,
+        required:true
+    },
+    observations:{
+        type:String
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    organization:{
+        type:String,
+        required:true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = mongoose.model("Taxi", PostSchema);
