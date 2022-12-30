efb9a311cd7936a5583733191637e46d7689acc2
victoria-parker <victoriaparkerb@gmail.com>
2022-12-30 13:57:26 +0000

    note model added

---
+++ models/Note.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const NoteSchema = new mongoose.Schema({
+    text:{
+        type:String,
+        required:true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    organization:{
+        type:String,
+        required:true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = mongoose.model("Note", PostSchema);
