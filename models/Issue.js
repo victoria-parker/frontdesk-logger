3e8b6bcd5082f99e3cbb4a7ec1d585e7af89ae13
victoria-parker <victoriaparkerb@gmail.com>
2023-01-04 11:31:20 +0000

    issue model added

---
+++ models/Issue.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const IssueSchema = new mongoose.Schema({
+    roomNumber:{
+        type: Number,
+        required:true
+    },
+    guestName:{
+        type: String,
+        required:true
+    },
+    text:{
+        type:String,
+        required:true
+    },
+    resolved:{
+        type:Boolean,
+        default: false
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    organization:{
+        type:String,
+        required:true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+module.exports = mongoose.model("Issue", PostSchema);
