b914ae54482f5da91cba94d5831ffc679db6372d
victoria-parker <victoriaparkerb@gmail.com>
2023-01-04 11:57:15 +0000

    main route added

---
+++ routes/main.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const router=express.Router();
+const homeController=require("../controllers/home")
+
+
+//Main Routes
+router.get("/",homeController.getIndex)
+
+module.exports=router;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express=require("express")
 const app=express()
-const mongoose=require("mongoose")
+// const mongoose=require("mongoose")
 const passport=require("passport")
 const session=require("express-session")
 // const MongoStore = require("connect-mongo")(session);
 const methodOverride=require("method-override")
 const flash=require("express-flash")
 const logger=require("morgan")
-const connectDB=require("./config/database")
+// const connectDB=require("./config/database")
+const mainRoutes=require("./routes/main")
 
 //dotenv config
 require("dotenv").config({path: "./config/config.env"})
@@ -16,13 +17,16 @@ require("dotenv").config({path: "./config/config.env"})
 require("./config/passport")(passport);
 
 //Connect To Database
-connectDB();
+// connectDB();
 
 //Using EJS for views
 app.set("view engine", "ejs");
 
 //Static Folder
 app.use(express.static("public"));
+
+//Setup Routes
+app.use("/", mainRoutes)
 
 //Server Running
 app.listen(process.env.PORT, () => {
