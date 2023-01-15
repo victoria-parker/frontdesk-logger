d44f988b980ae79bb1f55f9a65ff53b89b79b028
victoria-parker <victoriaparkerb@gmail.com>
2023-01-15 17:59:35 +0100

    log out added

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,6 +52,20 @@ exports.postLogin = (req,res,next) => {
     })(req, res, next);
 }
 
+//log out
+exports.logout = (req, res) => {
+  req.logout(() => {
+    console.log('User has logged out.')
+  })
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect("/");
+  });
+};
+
+
 //Get SignUp
 exports.getSignup = (req,res) =>{
     if(req.user){
