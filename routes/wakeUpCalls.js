31c7c5f20aa38d6c0672877e9d7139d7a53cceb2
victoria-parker <victoriaparkerb@gmail.com>
2023-01-17 23:33:30 +0100

    WakeUpCall routes added

--- routes/wakeUpCalls.js
+++ routes/wakeUpCalls.js
@@ -4,6 +4,10 @@ const wakeUpCallsController=require("../controllers/wakeUpCalls");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.post("/createWakeUpCall",ensureAuth,wakeUpCallsController.createWakeUpCall)
+router.get("/modifyWakeUpCall/:id",ensureAuth,wakeUpCallsController.getWakeUpCall)
+router.put("/modifyWakeUpCall/:id",ensureAuth,wakeUpCallsController.modifyWakeUpCall)
+router.get("/:id",ensureAuth,wakeUpCallsController.getWakeUpCall)
+router.put("/:id",ensureAuth,wakeUpCallsController.removeWakeUpCall)
 
 
 module.exports=router;
