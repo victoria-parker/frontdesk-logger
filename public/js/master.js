887119875ad90e0c16d0a5bfed3d239e2789a911
victoria-parker <victoriaparkerb@gmail.com>
2023-01-17 17:55:44 +0100

    swal corrected

--- public/js/master.js
+++ public/js/master.js
@@ -2,14 +2,14 @@ const removeForms=document.querySelectorAll('.removeForm')
 
 Array.from(removeForms).forEach(element=>element.addEventListener("submit",function(event){
     event.preventDefault()
-    let form = event.target.form;
+
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
+        confirmButtonColor: '#d9534f',
+        cancelButtonColor: '#5bc0de',
         confirmButtonText: 'Yes, delete it!'
       }).then((confirm)=>{
         if(confirm.value){
